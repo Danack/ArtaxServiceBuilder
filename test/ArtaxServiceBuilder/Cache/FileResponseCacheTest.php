f7f694141bc37550fc79c5b3b54372dde2bc8a2c
Danack <Danack@basereality.com>
2014-09-13 20:31:01 +0100

    Added note about test needed.

---
+++ test/ArtaxServiceBuilder/Cache/FileResponseCacheTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+class FileResponseCacheTest {
+
+    //Need to add a check that setting a 'Last-Modified' and/or Etag
+    //returns 'If-None-Match' or 'If-Modified-Since'
+    
+    //Add test that Vary entries are respected
+    //    Accept, Authorization, Cookie, X-GitHub-OTP
+    //    Accept-Encoding
+}
+
+ 
